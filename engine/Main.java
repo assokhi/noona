00f00d1882b6c9c504b8d7f461d42f3bfb39f628
assokhi <singhsokhiarvinder@gmail.com>
2026-01-31 23:45:03 +0530

    Add Board and Main classes for chess game initialization and input handling

---
+++ engine/Main.java
@@ -0,0 +1,13 @@
+package engine;
+import java.util.*;
+class Main{
+    public static void main (String[] args) {
+        System.out.println("noona :) started");
+        Scanner sc = new Scanner(System.in);
+        boolean checkmate = false;
+        while(!checkmate){
+            String move = sc.next();
+        }
+        sc.close();
+    }
+}
